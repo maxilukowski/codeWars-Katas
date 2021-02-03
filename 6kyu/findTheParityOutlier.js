62375a931141a556245e7e3b49d32312b3747328
maxilukowski <maxi.lukowski@yahoo.com>
2021-02-03 11:30:28 +0100

    add solution parityOutlier

--- 6kyu/findTheParityOutlier.js
+++ 6kyu/findTheParityOutlier.js
@@ -35,3 +35,9 @@ function findOutlier(integers) {
 }
 
 console.log(findOutlier(numbers))
+
+/* function findOutlier(int){
+  var even = int.filter(a=>a%2==0);
+  var odd = int.filter(a=>a%2!==0);
+  return even.length==1? even[0] : odd[0];
+} */
