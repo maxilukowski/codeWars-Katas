caa26d59f41f7576fc11ddea86c5b176ccd00d98
maxilukowski <maxi.lukowski@yahoo.com>
2021-03-14 13:34:27 +0100

    Add mumble

--- 7kyu/mumble.js
+++ 7kyu/mumble.js
@@ -21,3 +21,11 @@ function accum(s) {
 /* function accum(s) {
     return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
   } */
+
+function countPlaces(num) {
+  var sep = String(23.32).match(/\D/)[0]
+  var b = String(num).split(sep)
+  return b[1] ? b[1].length : 0
+}
+
+console.log(countPlaces(2.55))
